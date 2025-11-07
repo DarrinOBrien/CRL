03c8b16eb69aa24b1da149324ee14c7d44ea2ac0
Darrin O'Brien <darrinobrien5@gmail.com>
2025-11-06 18:50:09 -0800

    Fixed typo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ if ENVIRONMENT == "dev":
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECURITY_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
